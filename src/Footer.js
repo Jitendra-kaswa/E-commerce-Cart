5dcd6cc87b134bbcc76400f08c8cfd2308a0dc15
Jitendra Kaswa <kaswanjitendra89@gmail.com>
2021-05-29 14:23:38 +0530

    project completed

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = (props) => {
     return (
-        <div>
-            <h3> Total Price:- {props.totalPrice}</h3>
+        <div style={{fontSize:20, padding:20}}>
+            TOTAL: {props.totalPrice}
         </div>
     )
 }
